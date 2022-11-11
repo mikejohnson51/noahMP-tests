9d5bc859fb90d235d0dc90a64200ff028be50647
mikejohnson51 <jmj00@ucsb.edu>
2022-11-11 13:59:26 -0700

    path update

--- R/tracing_based_use_case.R
+++ R/tracing_based_use_case.R
@@ -1,13 +1,25 @@
-gages  = readRDS("/Users/mikejohnson/src-improvements/data/usgs_rc.rds") %>%
+pacman::p_load(sf, dplyr)
+
+pip = function(points, polygon, var){
+  st_join(polygon, points) %>%
+    st_drop_geometry() %>%
+    na.omit() %>%
+    count(get(var)) %>%
+    setNames(c(var, "n")) %>%
+    right_join(polygon, by = var) %>%
+    st_as_sf()
+}
+
+
+gages  = readRDS("data/usgs_rc.rds") %>%
   st_as_sf(coords = c("lon","lat"), crs = 4326) %>%
   st_transform(5070) %>%
   filter(startDate <= "2001-01-01", endDate >= "2018-01-01") %>%
   left_join(nhdplusTools::get_vaa(c("totdasqkm")), by = c("COMID" = "comid")) %>%
   filter(between(totdasqkm, 400, 1000))
 
-#1088
-
 basins = list()
+
 for(i in 1:nrow(gages)){
   out = dataRetrieval::findNLDI(comid = gages$COMID[i],
                           nav   = "UT",
@@ -22,29 +34,30 @@ for(i in 1:nrow(gages)){
   message(i)
 }
 
-#1088
 bas2 = dplyr::bind_rows(basins) %>%
   mutate(area  = st_area(.)/1e6) %>%
   mutate(gages = gages - 1) %>%
   filter(gages > 0) %>%
   st_transform(5070)
-#523
 
-niddata2 = niddata %>% select(NID_STORAGE, NID_HEIGHT, LONGITUDE, LATITUDE) %>%
-  filter(NID_HEIGHT >= 50 | NID_STORAGE > 10000) %>%
-  filter(!is.na(LATITUDE), !is.na(LONGITUDE)) %>%
-  st_as_sf(coords = c("LONGITUDE", "LATITUDE"), crs = 4236) %>%
+niddata = read_sf('https://nid.usace.army.mil/api/nation/gpkg')
+
+niddata2 = niddata %>%
+  select(nidStorage, nidHeight) %>%
+  filter(nidStorage >= 50 | nidStorage > 10000) %>%
   st_transform(st_crs(bas2))
 
 bas2 = pip(niddata2, bas2, "comid") %>%
   rename(dams = n) %>%
   mutate(dams = ifelse(is.na(dams), 0, dams)) %>%
   filter(dams == 0)
-# 185
 
-nlcd = raster::raster('/Volumes/Backup/NLCD/NLCD_2016_Land_Cover_L48_20190424/NLCD_2016_Land_Cover_L48_20190424.img')
-tmp2 = exactextractr::exact_extract(nlcd, bas2, function(values, coverage_fraction) table(substr(values,1,1)))
-out = dplyr::bind_rows(tmp2)
+nlcd = terra::rast('/Volumes/Backup/NLCD/NLCD_2016_Land_Cover_L48_20190424/NLCD_2016_Land_Cover_L48_20190424.img')
+tmp2 = exactextractr::exact_extract(nlcd,
+                                    bas2,
+                                    function(values, coverage_fraction) table(substr(values,1,1)))
+
+out = bind_rows(tmp2)
 tt = 100* out / rowSums(out, na.rm = TRUE)
 ttt = select(tt, ends_with('Freq'))
 ttt[is.na(ttt)] <- 0
@@ -52,15 +65,13 @@ ttt = round(ttt,2)
 ttt = setNames(ttt, c("water", "urban", "barren", "forest",
                       "shrub", "grassland", "agriculture", "wetland"))
 
-fin = bind_cols(bas2, ttt)
-fin = fin %>% filter(water <= 5, barren < 5, wetland < 5)
-# 118
+fin = bind_cols(bas2, ttt) %>%
+  filter(water <= 5, barren < 5, wetland < 5)
 
 fin$lc10    = rowSums(st_drop_geometry(fin[,7:14]) > 10, na.rm = T)
 fin$lcMax   = apply(st_drop_geometry(fin[,8:16]), 1, function(x) max(x))
 
 f2 = filter(fin, lc10 >= 3, lcMax < 55)
-# 16
 f2$class = NA
 
 for(i in 1:nrow(f2)){
@@ -68,6 +79,5 @@ for(i in 1:nrow(f2)){
     sort(names(f2)[order(st_drop_geometry(f2[i,8:16]), decreasing = TRUE)[1:3] + 7]),
     collapse = "-")
 }
-table(f2$class)
-table(f2$gages)
-mapview::mapview(mike, zcol = "class", col.regions = RColorBrewer::brewer.pal(4, "Dark2"))
+
+
